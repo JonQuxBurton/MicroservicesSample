7e0f38aab1a95bf3ad19fe752f2caf9718314bbe
JonQuxBurton <jonburto@gmail.com>
2018-04-02 16:49:15 +0100

    Added Status modules.

---
+++ src/PhoneLineOrderer/StatusModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace PhoneLineOrderer
+{
+    public class StatusModule : NancyModule
+    {
+        public StatusModule() : base("/status")
+        {
+            Get("", x => {
+                return HttpStatusCode.OK;
+            });
+        }
+    }
+}
---
+++ src/SmsSender/StatusModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace SmsSender
+{
+    public class StatusModule : NancyModule
+    {
+        public StatusModule() : base("/status")
+        {
+            Get("", x => {
+                return HttpStatusCode.OK;
+            });
+        }
+    }
+}
